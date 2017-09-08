9628ef47f05803bdf593d4dfce4b6b65e5143cc7
Giwan Persaud <giwan@knowble.nl>
2017-09-08 11:32:48 +0200

    intermediate webpack update

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
 import Clicker from 'Clicker';
+import axios from 'axios';
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-  }
 
   render () {
     return(
       <div>
         <h1>Hello {this.props.name}</h1>
         <Clicker />
+        <Login />
       </div>
     );
   }
+}
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                <input type="text" ref="username"></input>
+                <input type="password" ref="password"></input>
+                <button onClick={this.performLogin.bind(this)}>Login</button>
+            </div>
+        )
+    }
+
+    performLogin() {
+        axios.get("/login.json", {
+            params: {
+                username: this.refs.username.value,
+                password: this.refs.password.value
+            }
+        }).then(rsp => {
+            return console.log('successful response: ', rsp);
+        }).catch(err => console.log("failed ", err));
+    }
 }
